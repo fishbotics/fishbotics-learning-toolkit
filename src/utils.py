36b7e198fda7829e66db362e53b593b120c8ad7f
Adam Fishman <45679966+fishbotics@users.noreply.github.com>
2021-01-11 11:13:17 -0800

    Update utils.py
    Added back find_replace_config_strings function

--- src/utils.py
+++ src/utils.py
@@ -3,3 +3,16 @@ def send_batch_to_device(batch, keys, device):
     for key in keys:
         device_batch[key] = batch[key].to(device)
     return device_batch
+
+def find_replace_config_strings(config, replacements):
+    for e, v in config.items():
+        if isinstance(v, str):
+            orig_path = v
+            for orig, new in replacements.items():
+                path = v.replace(orig, new)
+                if path != orig_path:
+                    config[e] = path
+                    break
+        elif isinstance(v, dict):
+            find_replace_config_strings(v, replacements)
+    return config
